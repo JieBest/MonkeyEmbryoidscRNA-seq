4fd1206d476671855b35dc681baa1fc3692ce184
Jie Li <89961915+JieBest@users.noreply.github.com>
2025-05-20 14:02:04 +0800

    Create MonkeyEmbryoid_HumanPCW3_integration.R

---
+++ 03.Integration/MonkeyEmbryoid_HumanPCW3_integration.R
@@ -0,0 +1,3 @@
+#This script integrate human embryo CS7 dataset (Tyser et al., 2021) and human gastruloid at 24hr
+#Link to human embryo CS7 dataset is provided in https://shendure-web.gs.washington.edu/content/members/hGastruloid_website/public/
+#Human gastrloid datasets from this paper are provided at National Center for Biotechnology Information (NCBI) Gene Expression Omnibus (GEO) under accession numbers GSE208369
