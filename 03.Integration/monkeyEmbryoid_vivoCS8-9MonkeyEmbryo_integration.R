403d6312324c8d1a3d79d197c9e46c625b7301b3
Jie Li <89961915+JieBest@users.noreply.github.com>
2025-05-20 13:58:26 +0800

    Create monkeyEmbryoid_vivoCS8-9MonkeyEmbryo_integration.R

---
+++ 03.Integration/monkeyEmbryoid_vivoCS8-9MonkeyEmbryo_integration.R
@@ -0,0 +1,11 @@
+#This script integrate monkey embryo CS8-9 dataset (Zhai et al., 2022) and monkey embryoid
+#Link to monkey embryo CS8-9 dataset is provided at National Center for Biotechnology Information (NCBI) Gene Expression Omnibus (GEO) under accession numbers GSE208369
+#Human gastrloid datasets from this paper are provided at National Center for Biotechnology Information (NCBI) Gene Expression Omnibus (GEO) under accession numbers GSE208369
+
+################# 1. load required packages
+#################
+#################
+#################
+#################
+#################
+
